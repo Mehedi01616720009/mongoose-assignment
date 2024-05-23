feaff75305718946bb1d4b35897fea66ed76df7e
Mehedi01616720009 <mehedi01616720009@gmail.com>
2024-05-23 16:37:44 +0600

    read me

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Mongoose Express depth assignment project
+
+# Steps of setup this project locally
+
+1. clone the github repository
+2. run "npm install" command
+3. create a ".env" file in root directory and store { NODE_ENV, PORT, DATABASE_URL }
+4. create a dist folder in root directory
+5. run "npm run build" command
+6. run "npm run start" command
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import { ProductRoute } from './app/modules/product/product.route'
+import { OrderRoute } from './app/modules/order/order.route'
 const app: Application = express()
 
 app.use(express.json())
@@ -8,5 +9,12 @@ app.use(cors())
 
 // product route
 app.use('/api/products', ProductRoute)
+
+// order route
+app.use('/api/orders', OrderRoute)
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({ message: 'server is running' })
+})
 
 export default app
